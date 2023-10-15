633911c36032bce4c2b4f1c8424b89db3bf9ef55
andreSantos1975 <andredev1975@gmail.com>
2023-10-15 06:31:46 -0300

    .env gitignore

--- server copy.js
+++ server copy.js
@@ -1,4 +1,4 @@
-// server.js
+// server/server.js
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
@@ -10,13 +10,32 @@ app.use(express.json());
 app.use(cors()); // Habilita o CORS para todas as rotas
 const accessToken = process.env.REACT_APP_API_TOKEN_PRINTFUL;
 
+// Rota para obter informações sobre uma categoria específica
+app.get('/printful/categories/:id', async (req, res) => {
+  try {
+    const categoryId = req.params.id;
 
+    const response = await axios.get(`https://api.printful.com/categories/${categoryId}`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
 
+    const categoryInfo = response.data.result;
 
+    res.json(categoryInfo);
+  } catch (error) {
+    console.error('Erro ao buscar informações da categoria:', error);
+    // Se a categoria não for encontrada ou ocorrer algum erro, você pode enviar um status 404
+    res.status(404).json({ error: 'Categoria não encontrada' });
+  }
+});
+
+// Rota para obter informações de produtos de uma loja
 app.post('/printful/stores', async (req, res) => {
   try {
     const { productIds } = req.body;
-    console.log('ProductId no server', productIds);//---------------------------------------------log
+    console.log('ProductId no server', productIds);//-------------------------------------------log
     const productInfoArray = [];
 
     for (const productId of productIds) {
@@ -39,3 +58,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor intermediário rodando na porta ${PORT}`);
 });
+
+//----------------------------------------------------------------
